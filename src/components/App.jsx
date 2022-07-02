cf97d177f3884acf2445184703e7266c1e491d21
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-02 18:08:21 -0400

    change in state for header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // jset the constant, set the function to use when changing state and set the starting state value useState starting text
+  const [headingText, setHeadingText] = useState("Hello");
+  function handleClick() {
+    console.log("clicked");
+    // use the function declared to when state changes
+    setHeadingText("Submitted");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
