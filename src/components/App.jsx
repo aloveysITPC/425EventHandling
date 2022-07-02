854037c5f8f5010d4f83dc4749930867b41c08e0
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-02 19:03:39 -0400

    UseStateToChange

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,33 @@ import React, { useState } from "react";
 function App() {
   // jset the constant, set the function to use when changing state and set the starting state value useState starting text
   const [headingText, setHeadingText] = useState("Hello");
+  const [bkgColor, changeColor] = useState(false);
   function handleClick() {
     console.log("clicked");
     // use the function declared to when state changes
     setHeadingText("Submitted");
   }
+  function goBlack() {
+    console.log("mouse over");
+    changeColor(true);
+    console.log({ bkgColor });
+  }
 
+  function goWhite() {
+    changeColor(false);
+  }
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        style={{ backgroundColor: bkgColor ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={goBlack}
+        onMouseOut={goWhite}
+      >
+        Submit
+      </button>
     </div>
   );
 }
